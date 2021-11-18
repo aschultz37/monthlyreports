700234850940a7ddb3a1268581ade6b86877c0e0
austinc-s <austinc_schultz@dfci.harvard.edu>
2021-11-18 16:38:53 -0500

    adding studylist header for use w/ timepointSort

---
+++ studylist.hpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <vector>
+
+namespace SL{
+    std::vector <std::string> studyNums = {"20028", "17455", "13325", "18394", "18634", "20068", 
+                                        "18617", "20166", "20153", "20371", "19672", "93085"};
+    std::vector <std::vector <std::string>> timepoints = {{},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {},
+                                                        {}};
+}
